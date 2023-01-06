03950a5d7c57e3b4ffc240a1188d5dfab20a52b0
Aiden Bai <aiden.bai05@gmail.com>
2023-01-05 23:16:38 -0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         index: new URL('./index.html', import.meta.url).pathname,
-        advice: new URL('./advice.html', import.meta.url).pathname,
+        '404': new URL('./404.html', import.meta.url).pathname,
       },
     },
   },
